4f2441a74cb7134035268f710adab43f76135aee
Anselm Joseph <anselmjs@icloud.com>
2024-05-02 12:21:34 -0500

    cors and updated request methods

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,34 @@
-import cors from '@koa/cors';
-import Koa from 'koa';
-import bodyParser from 'koa-bodyparser';
-import helmet from 'koa-helmet';
-import koaLogger from 'koa-logger';
-import error from './middleware/error';
-import openapi from './middleware/openapi';
-import router from './router';
-import logger from './utils/logger';
+import cors from "@koa/cors";
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import helmet from "koa-helmet";
+import koaLogger from "koa-logger";
+import error from "./middleware/error";
+import openapi from "./middleware/openapi";
+import router from "./router";
+import logger from "./utils/logger";
 
 const app = new Koa();
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(openapi());
+if (process.env.NODE_ENV !== "production") {
+	app.use(openapi());
 }
 
 app.use(
-  koaLogger({
-    transporter: (str, args) => {
-      logger.info(str);
-    },
-  }),
+	koaLogger({
+		transporter: (str, args) => {
+			logger.info(str);
+		},
+	})
 );
 app.use(helmet());
 app.use(
-  cors({
-    origin: '*',
-  }),
+	cors({
+		origin: "http://localhost:3000", // replace with your client app's URL
+		allowMethods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+		allowHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
+	})
 );
 app.use(bodyParser());
 app.use(error());
--- src/controller/chat.ts
+++ src/controller/chat.ts
@@ -1,92 +1,92 @@
-import Joi from 'joi';
-import { DefaultContext, Next } from 'koa';
-import { ChatFactory } from '../lib/chat';
-import { verifyJWT } from '../middleware/auth';
-import { checkUserOwnsChat } from '../middleware/security';
-import { resource } from '../utils/resource';
-import { Methods } from './types';
+import Joi from "joi";
+import { DefaultContext, Next } from "koa";
+import { ChatFactory } from "../lib/chat";
+import { verifyJWT } from "../middleware/auth";
+import { checkUserOwnsChat } from "../middleware/security";
+import { resource } from "../utils/resource";
+import { Methods } from "./types";
 
 export async function createNewChat(ctx: DefaultContext, next: Next) {
-  ctx.body = await ChatFactory.create(ctx.user);
-  next();
+	ctx.body = await ChatFactory.create(ctx.user);
+	next();
 }
 
 export async function getChatSources(ctx: DefaultContext, next: Next) {
-  ctx.body = await ChatFactory.getSources(ctx.request.body.id);
-  next();
+	ctx.body = await ChatFactory.getSources(ctx.request.body.id);
+	next();
 }
 
 export async function getChat(ctx: DefaultContext, next: Next) {
-  ctx.body = await ChatFactory.getById(ctx.request.body.id);
-  next();
+	ctx.body = await ChatFactory.getById(ctx.request.body.id);
+	next();
 }
 
 export async function deleteChat(ctx: DefaultContext, next: Next) {
-  ctx.body = await ChatFactory.delete(ctx.request.body.id);
-  next();
+	ctx.body = await ChatFactory.delete(ctx.request.body.id);
+	next();
 }
 
 export async function queryChat(ctx: DefaultContext, next: Next) {
-  ctx.body = await ChatFactory.query(ctx.request.body);
-  next();
+	ctx.body = await ChatFactory.query(ctx.request.body);
+	next();
 }
 
 export async function getConversations(ctx: DefaultContext, next: Next) {
-  ctx.body = await ChatFactory.getConversations(ctx.request.body.id);
-  next();
+	ctx.body = await ChatFactory.getConversations(ctx.request.body.id);
+	next();
 }
 
 export default resource([
-  {
-    path: '/',
-    controller: createNewChat,
-    method: Methods.POST,
-    security: [verifyJWT],
-  },
-  {
-    path: '/sources',
-    controller: getChatSources,
-    method: Methods.GET,
-    security: [verifyJWT, checkUserOwnsChat],
-    schema: Joi.object({
-      id: Joi.string().required(),
-    }),
-  },
-  {
-    path: '/',
-    controller: getChat,
-    method: Methods.GET,
-    schema: Joi.object({
-      id: Joi.string().required(),
-    }),
-    security: [verifyJWT, checkUserOwnsChat],
-  },
-  {
-    path: '/',
-    controller: deleteChat,
-    method: Methods.DELETE,
-    schema: Joi.object({
-      id: Joi.string().required(),
-    }),
-    security: [verifyJWT, checkUserOwnsChat],
-  },
-  {
-    path: '/query',
-    controller: queryChat,
-    method: Methods.POST,
-    schema: Joi.object({
-      id: Joi.string().required(),
-      query: Joi.string().required(),
-    }),
-    security: [verifyJWT, checkUserOwnsChat],
-  },
-  {
-    path: '/conversations',
-    controller: getConversations,
-    method: Methods.GET,
-    schema: Joi.object({
-      id: Joi.string().required(),
-    }),
-    security: [verifyJWT, checkUserOwnsChat],
-  },
+	{
+		path: "/",
+		controller: createNewChat,
+		method: Methods.PUT,
+		security: [verifyJWT],
+	},
+	{
+		path: "/sources",
+		controller: getChatSources,
+		method: Methods.POST,
+		security: [verifyJWT, checkUserOwnsChat],
+		schema: Joi.object({
+			id: Joi.string().required(),
+		}),
+	},
+	{
+		path: "/",
+		controller: getChat,
+		method: Methods.POST,
+		schema: Joi.object({
+			id: Joi.string().required(),
+		}),
+		security: [verifyJWT, checkUserOwnsChat],
+	},
+	{
+		path: "/",
+		controller: deleteChat,
+		method: Methods.DELETE,
+		schema: Joi.object({
+			id: Joi.string().required(),
+		}),
+		security: [verifyJWT, checkUserOwnsChat],
+	},
+	{
+		path: "/query",
+		controller: queryChat,
+		method: Methods.POST,
+		schema: Joi.object({
+			id: Joi.string().required(),
+			query: Joi.string().required(),
+		}),
+		security: [verifyJWT, checkUserOwnsChat],
+	},
+	{
+		path: "/conversations",
+		controller: getConversations,
+		method: Methods.GET,
+		schema: Joi.object({
+			id: Joi.string().required(),
+		}),
+		security: [verifyJWT, checkUserOwnsChat],
+	},
 ]);
