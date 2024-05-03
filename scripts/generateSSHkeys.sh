36f3844adffd815d923f3636ff604be5fc3f8057
Anshul Sinha <asinha@teamliquid.com>
2024-05-03 10:25:38 +0530

    minor fix

--- scripts/generateSSHkeys.sh
+++ scripts/generateSSHkeys.sh
@@ -1,5 +1,5 @@
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650
-rm -rf ./cert
-mkdir ./cert
-mv ./cert.pem ./cert/cert.pem
-mv ./key.pem ./cert/key.pem
+rm -rf ./certs
+mkdir ./certs
+mv ./cert.pem ./certs/cert.pem
+mv ./key.pem ./certs/key.pem
